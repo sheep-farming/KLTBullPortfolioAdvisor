b9f87970253b5869a5a19f411e1230178cc4c146
DuPupu <zouluoyi@gmail.com>
2015-05-03 00:16:24 +0800

    new fetching method

--- FetchDataFromYahoo.m
+++ FetchDataFromYahoo.m
@@ -9,14 +9,21 @@ DateLength=90;
 % Fetch ticker data from Yahoo
 for(i=1:length(ticker))
     try
-    data=fetch(yahoo,ticker(i),'Adj Close',now-DateLength,now,SamplePeriod);
-    %data=get_yahoo_stockdata2(ticker{i},now-DateLength,now,SamplePeriod,1);
-    %data=[datenum(data.Date) data.AdjClose];
-    Stock(i).name=ticker(i);
-    Stock(i).number=(ticker(i));
-    Stock(i).price=flipud(data);%Now->Old => Old to Now.
-    Stock(i).returns=price2ret(Stock(i).price(:,2),Stock(i).price(:,1))*22; %Daily Return Rate. convert to annual ~ returns*365
-            %Progress Bar
+        try
+            data=fetch(yahoos,ticker(i),'Adj Close',now-DateLength,now,SamplePeriod);
+            Stock(i).name=ticker(i);
+            Stock(i).number=(ticker(i));
+            Stock(i).price=flipud(data);%Now->Old => Old to Now.
+            Stock(i).returns=price2ret(Stock(i).price(:,2),Stock(i).price(:,1))*22; %Daily Return Rate. convert to annual ~ returns*365
+        catch %IF DON'T HAVE DATA FETCH TOOLBOX
+            data=get_yahoo_stockdata2(ticker{i},now-DateLength,now,SamplePeriod,1);
+            data=[datenum(data.Date) data.AdjClose];
+            Stock(i).name=ticker(i);
+            Stock(i).number=(ticker(i));
+            Stock(i).price=data;
+            Stock(i).returns=price2ret(Stock(i).price(:,2),Stock(i).price(:,1))*22; %Daily Return Rate. convert to annual ~ returns*365
+      end
+           %Progress Bar
             fetchProgress=(60*i/(1.0*length(ticker)));
             clc;
             fprintf('\nDownloading data from Yahoo!...\n\n\n  ');
